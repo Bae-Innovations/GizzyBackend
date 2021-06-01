6267bea514b1ac3e52b9fa1f5cf4c7c0c7aa4eb0
Imtiaz <ak.imtiaz147@gmail.com>
2021-06-01 18:17:46 +0600

    added cors policy to allow all

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // imports
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors')
 
 const logger = require('./logger/logger');
 const authenticateToken = require('./middleware');
@@ -16,10 +17,18 @@ mongoose.connect('mongodb+srv://gizzy:cftXDR456@cluster0.az372.mongodb.net/testd
   { useNewUrlParser: true, useUnifiedTopology: true },
   () => logger.info('connected to DB!'));
 
+// defining cors policy
+// let corsOptions = {
+//   origin: 'http://localhost:8080',
+//   optionsSuccessStatus: 200 // For legacy browser support
+// }
 
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// app.use(cors(corsOptions));
+app.use(cors())
+
 
 // routes
 app.get('/', (req, res) => {
