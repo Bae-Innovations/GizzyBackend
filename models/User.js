86ccf0108788d21c6b2f00bf6a12e009ce30edeb
Imtiaz Ahmed Khan <ak.imtiaz147@gmail.com>
2021-06-18 02:39:51 +0600

    wip

--- models/User.js
+++ models/User.js
@@ -37,6 +37,10 @@ const UserSchema = mongoose.Schema({
     type: Number,
     default: 0,
   },
+  earlyAdopter: {
+    type: Boolean,
+    default: false
+  }
   // liked: {
   //    type:
   // }
--- utils/addIPFSjson.js
+++ utils/addIPFSjson.js
@@ -8,4 +8,35 @@ const addIPFSjson = async (meta) => {
   return cid.path;
 };
 
+data = {
+  name:"Promo Gizzy",
+  description:"A very special and rare unbreedable gizzy. This will only be given to the 1000 early adopters and never be minted again.",
+  image:"https://ipfs.io/ipfs/QmQkQBnewjrFox48QEgKJHW6DLtmk4XiZ2Hg5FrtsY4VAw",
+  attributes: [
+    {
+      trait_type: "Strength", 
+      value: "3"
+    }, 
+    {
+      trait_type: "Constituion", 
+      value: "3"
+    },
+    {
+      trait_type: "Restoration", 
+      value: "3"
+    },
+    {
+      trait_type: "Charisma", 
+      value: "3"
+    },
+    {
+      trait_type: "Lycano", 
+      value: "Normal"
+    },
+  ]
+}
+
+addIPFSjson(data)
+.then((response) => console.log(response))
+
 module.exports = addIPFSjson;
