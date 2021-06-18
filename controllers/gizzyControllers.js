cdebc61b6c7295a16391291f7a53154bf88e68f3
Imtiaz <ak.imtiaz147@gmail.com>
2021-06-18 06:35:12 +0600

    bug fix

--- controllers/gizzyControllers.js
+++ controllers/gizzyControllers.js
@@ -160,7 +160,7 @@ const boughtEgg = async (req, res) => {
     try{
         user = await UserSchema.findOne({publicAddress: res.locals.publicAddress})
         amount = await amountOfGizzy(publicAddress)
-        if (amount >= 2 && user.earlyAdopter != true){
+        if (amount >= 2 && user?.earlyAdopter != true){
             logger.debug("entered if statement: user is supposed to get promo gizzy")
             user.earlyAdopter = true;
             await user.save();
